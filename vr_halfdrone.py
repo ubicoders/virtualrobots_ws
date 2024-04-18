3d725e1b3cc6f78bfcf697d69d14eb30244a6db2
Hongyun Lee (Elliot) <hylee101001@gmail.com>
2024-04-18 10:55:46 -0700

    aaa

--- vr_halfdrone.py
+++ vr_halfdrone.py
@@ -7,21 +7,21 @@ halfdrone = GeneralRobot()
 class UbicodersMain:
     def __init__(self) -> None:
         # Define the variables here!
-        pass    
+        pass
     
     def setup(self):
         # This API will be released soon!
-        # mr.mass = 2 #kg
         pass
 
     def loop(self):
         # Print the multirotor states from the simulator!
         states = halfdrone.states
-
+        
+        ts = states.timestamp
         theta = states.euler.x # the angle of the half drone
         theta_dot = -states.ang_vel.x # the angular velocity of the half drone
         throttle = states.pwm[0] # the current throttle of the half drone
-        print(f"theta: {theta}, theta_dot: {theta_dot}, throttle: {throttle}")
+        print(f"timestamp: {ts}, theta: {theta}, theta_dot: {theta_dot}, throttle: {throttle}")
 
         # Set the PWM values to the motors! between 1100 to 2000
         halfdrone.set_pwm(m0=1234, m1=1234, m2=0, m3=0)
